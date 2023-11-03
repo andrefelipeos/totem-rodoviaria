f05647b19b836c9d8138fad5186bd319fd333520
André Sousa <andre.felipe.2048@gmail.com>
2023-11-02 21:15:10 -0300

    Esboça menu de manutenção e marca demandas na documentação

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@
 
 using namespace std;
 
-const char* SENHA_PARA_MANUTENCAO = "senhafraca";
+const char *SENHA_PARA_MANUTENCAO = "senhafraca";
 
 void OpcoesDeManutencao();
 
+ListaEncadeada linhas_de_onibus;
+
 int main()
 {
-    ListaEncadeada linhas_de_onibus;
 
     cout << "O que você deseja fazer?" << endl;
     cout << "  1. Procurar linha de ônibus" << endl;
@@ -29,9 +30,41 @@ int main()
     default:
         break;
     }
+
+    linhas_de_onibus.ImprimeDadosFormatados();
+
+    return 0;
 }
 
 void OpcoesDeManutencao()
 {
-    // incluir, alterar e eliminar linhas e paradas
+    cout << "O que você deseja fazer?" << endl;
+    cout << "  1. Adicionar uma linha de ônibus" << endl;
+    cout << "  2. Alterar uma linha de ônibus" << endl;
+    cout << "  3. Remover uma linha de ônibus" << endl;
+    cout << "  4. Adicionar uma parada em uma linha" << endl;
+    cout << "  5. Alterar uma parada em uma linha" << endl;
+    cout << "  6. Remover uma parada em uma linha" << endl;
+
+    int escolha;
+    cin >> escolha;
+    switch (escolha)
+    {
+    case 1:
+        /* adicionar linha */
+        int numero;
+        //string companhia;
+        cin >> numero;
+        //cin >> companhia;
+        linhas_de_onibus.InsereLinhaNoInicio(numero, "companhia");
+        break;
+    case 2:
+        /* alterar linha */
+        break;
+    case 3:
+        /* remover linha */
+        break;
+    default:
+        break;
+    }
 }
