45710b0720407886b13d0a5fcad918e280683393
André Sousa <andre.felipe.2048@gmail.com>
2023-11-03 11:46:46 -0300

    Cria menus principal e de manutenção do programa

--- main.cpp
+++ main.cpp
@@ -8,63 +8,85 @@ using namespace std;
 const char *SENHA_PARA_MANUTENCAO = "senhafraca";
 
 void OpcoesDeManutencao();
+void RealizarManutencao();
+void ProcurarLinhaDeOnibus();
 
 ListaEncadeada linhas_de_onibus;
 
 int main()
 {
-
-    cout << "O que você deseja fazer?" << endl;
-    cout << "  1. Procurar linha de ônibus" << endl;
-    cout << "  2. Manutenção do programa" << endl;
-
-    int escolha;
-    cin >> escolha;
-    switch (escolha)
+    int escolha = -1;
+    do
     {
-    case 1:
-        break;
-    case 2:
-        OpcoesDeManutencao();
-        break;
-    default:
-        break;
-    }
+        cout << "O que você deseja fazer?" << endl;
+        cout << "  0. Encerrar o programa e sair" << endl;
+        cout << "  1. Procurar uma linha de ônibus" << endl;
+        cout << "  2. Realizar manutenção do programa" << endl;
+
+        cin >> escolha;
+        switch (escolha)
+        {
+        case 1:
+            ProcurarLinhaDeOnibus();
+            break;
+        case 2:
+            RealizarManutencao();
+            break;
+        default:
+            break;
+        }
+    } while (escolha != 0);
 
     linhas_de_onibus.ImprimeDadosFormatados();
 
     return 0;
 }
 
+void ProcurarLinhaDeOnibus()
+{
+    cout << "Essa função ainda não foi implementada..." << endl;
+}
+
 void OpcoesDeManutencao()
 {
     cout << "O que você deseja fazer?" << endl;
+    cout << "  0. Encerrar manutenção do programa" << endl;
     cout << "  1. Adicionar uma linha de ônibus" << endl;
     cout << "  2. Alterar uma linha de ônibus" << endl;
     cout << "  3. Remover uma linha de ônibus" << endl;
     cout << "  4. Adicionar uma parada em uma linha" << endl;
     cout << "  5. Alterar uma parada em uma linha" << endl;
     cout << "  6. Remover uma parada em uma linha" << endl;
+}
 
-    int escolha;
-    cin >> escolha;
-    switch (escolha)
+void RealizarManutencao()
+{
+    int escolha = -1;
+    do
     {
-    case 1:
-        /* adicionar linha */
-        int numero;
-        //string companhia;
-        cin >> numero;
-        //cin >> companhia;
-        linhas_de_onibus.InsereLinhaNoInicio(numero, "companhia");
-        break;
-    case 2:
-        /* alterar linha */
-        break;
-    case 3:
-        /* remover linha */
-        break;
-    default:
-        break;
-    }
+        OpcoesDeManutencao();
+        cin >> escolha;
+        switch (escolha)
+        {
+        case 1:
+        {
+            int numero;
+            string companhia;
+            cout << "Número da linha: ";
+            cin >> numero;
+            cout << "Nome da companhia: ";
+            getline(cin >> ws, companhia);
+            linhas_de_onibus.AdicionaNovaLinhaOrdenadamente(numero, companhia);
+            break;
+        }
+        case 2:
+            /* alterar linha */
+            break;
+        case 3:
+            /* remover linha */
+            break;
+        default:
+            break;
+        }
+    } while (escolha != 0);
 }
