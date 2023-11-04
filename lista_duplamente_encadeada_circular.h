31b7886222dbb8ed6b9d3600866227a383ef2dfa
André Sousa <andre.felipe.2048@gmail.com>
2023-11-04 12:08:29 -0300

    Inicia implementação de métodos para adicionar uma parada numa linha

--- lista_duplamente_encadeada_circular.h
+++ lista_duplamente_encadeada_circular.h
@@ -23,12 +23,56 @@ private:
 public:
     ListaDuplamenteEncadeadaCircular() = default;
 
-    void ImprimeDadosFormatados()
+    /*
+    Incluir uma parada numa linha. Neste ponto o usuário deve dizer qual o número da
+linha onde quer incluir uma parada e receberá na tela uma lista numerada de todas as
+paradas existentes nesta lista. Em seguida ele digita o número da parada após a qual
+ele quer inserir uma parada e inserirá a parada.
+    */
+    void AdicionaParadaAposIndice(unsigned indice_anterior, string nome_da_parada, string horario_chegada, string horario_de_partida)
+    {
+        // buscar parada anterior ao ponto em que a nova parada será adicionada
+        // criar e adicionar uma nova parada após a parada anteriro
+        if (indice_anterior > this->tamanho_)
+        {
+            cout << "Índice de parada fora do intervalo de paradas." << endl;
+            return;
+        }
+
+        // dá para fazer mais rápido caso o usuário escolha inserir após última parada
+        // e se ele quiser inserir no início, como faz?
+
+        NoDuplamenteEncadeavel *no_iterador = nullptr;
+        if (indice_anterior == this->tamanho_)
+        {
+            no_iterador = this->ultimo_no_;
+        }
+        else
+        {
+            no_iterador = this->primeiro_no_;
+            unsigned counter = 1;
+            while (counter < indice_anterior)
+            {
+                no_iterador = no_iterador->proximo_no;
+                counter++;
+            }
+        }
+
+        NoDuplamenteEncadeavel *nova_parada = new NoDuplamenteEncadeavel(nome_da_parada, horario_de_partida, horario_chegada);
+        nova_parada->no_anterior = no_iterador;
+        nova_parada->proximo_no = no_iterador->proximo_no;
+        no_iterador->proximo_no->no_anterior = nova_parada;
+        no_iterador->proximo_no = nova_parada;
+    }
+
+    void ImprimeParadasNumeradas() const
     {
         NoDuplamenteEncadeavel *no_iterador = this->primeiro_no_;
+        unsigned contador = 1;
         while (no_iterador != nullptr)
         {
             cout << "-----\n";
+            cout << "Parada " << contador++;
             cout << "Nome da parada: " << no_iterador->nome_da_parada << "\n";
             cout << "Horário de chegada: " << no_iterador->horario_de_chegada << "\n";
             cout << "Horário de partida: " << no_iterador->horario_de_partida << "\n";
@@ -53,7 +97,12 @@ public:
         }
     }
 
-    ~ListaEncadeada()
+    unsigned RecuperaTamanho() const
+    {
+        return this->tamanho_;
+    }
+
+    ~ListaDuplamenteEncadeadaCircular()
     {
         EsvaziarLista();
     }
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 const char *SENHA_PARA_MANUTENCAO = "senhafraca";
 
+void AdicionarParada();
 void MostrarTodasAsLinhas();
 void OpcoesDeManutencao();
 void RealizarManutencao();
@@ -110,10 +111,58 @@ void RealizarManutencao()
         case 3:
             RemoverLinha();
             break;
+        case 4:
+            AdicionarParada();
+            break;
         default:
             break;
         }
     } while (escolha != 0);
+}
+
+/*
+Incluir uma parada numa linha. Neste ponto o usuário deve dizer qual o número da
+linha onde quer incluir uma parada e receberá na tela uma lista numerada de todas as
+paradas existentes nesta lista. Em seguida ele digita o número da parada após a qual
+ele quer inserir uma parada e inserirá a parada.
+*/
+void AdicionarParada()
+{
+    cout << "Número da linha em que a parada será inserida: ";
+    unsigned numero_da_linha;
+    cin >> numero_da_linha;
+    // buscar uma linha pelo número, e acessar sua lista de paradas
+    NoEncadeavel *linha = linhas_de_onibus.BuscaLinhaPeloNumero(numero_da_linha);
+    if (linha == nullptr)
+    {
+        cout << "Indique uma linha cadastrada." << endl;
+        return;
+    }
+    //cout << "Linha: " << linha->numero_da_linha << endl;
+    //cout << "Companhia: " << linha->nome_da_companhia << endl;
+    linha->paradas->ImprimeParadasNumeradas();
+    // Em seguida ele digita o número da parada após a qual ele quer inserir
+    // uma parada e inserirá a parada.
+    cout << "Após qual parada você deseja adicionar a nova parada? " << endl;
+    //cout << "Digite 0 para inserir a nova parada no início da lista." << endl;
+    cout << "Índice anterior: ";
+    unsigned indice_anterior;
+    cin >> indice_anterior;
+    if (indice_anterior > linha->paradas->RecuperaTamanho())
+    {
+        cout << "Existem apenas " << linha->paradas->RecuperaTamanho() << "." << endl;
+        return;
+    }
+    string nome_da_parada;
+    string horario_de_chegada;
+    string horario_de_partida;
+    cout << "Nome da nova parada: " << endl;
+    getline(cin >> ws, nome_da_parada);
+    cout << "Horário de chegada: " << endl;
+    getline(cin >> ws, horario_de_chegada);
+    cout << "Horário de partida: " << endl;
+    getline(cin >> ws, horario_de_partida);
+    linha->paradas->AdicionaParadaAposIndice(indice_anterior, nome_da_parada, horario_de_chegada, horario_de_partida);
 }
 
 void RemoverLinha()
