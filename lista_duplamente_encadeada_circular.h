3f3deb7bb533abde40f9ff0b0587b4ae7aa40b18
André Sousa <andre.felipe.2048@gmail.com>
2023-11-04 13:20:40 -0300

    Adiciona método AdicionaPrimeiraParada e usa único nó sentinela

--- lista_duplamente_encadeada_circular.h
+++ lista_duplamente_encadeada_circular.h
@@ -9,53 +9,37 @@
 using namespace std;
 
 /*
-    Uma lista duplamente encadeada circular, para cada linha de ônibus, onde
-    cada elemento dessa lista possui dados referentes a uma parada do ônibus.
+    Uma lista duplamente encadeada circular (...) onde cada elemento dessa
+    lista possui dados referentes a uma parada do ônibus.
 */
 
 class ListaDuplamenteEncadeadaCircular
 {
 private:
-    NoDuplamenteEncadeavel *primeiro_no_ = nullptr;
-    NoDuplamenteEncadeavel *ultimo_no_ = nullptr;
+    NoDuplamenteEncadeavel *no_sentinela_ = nullptr;
     unsigned tamanho_ = 0;
 
 public:
     ListaDuplamenteEncadeadaCircular() = default;
 
     /*
-    Incluir uma parada numa linha. Neste ponto o usuário deve dizer qual o número da
-linha onde quer incluir uma parada e receberá na tela uma lista numerada de todas as
-paradas existentes nesta lista. Em seguida ele digita o número da parada após a qual
-ele quer inserir uma parada e inserirá a parada.
+    Incluir uma parada numa linha. (...) o usuário (...) digita o número da
+    parada após a qual ele quer inserir uma parada (...).
     */
     void AdicionaParadaAposIndice(unsigned indice_anterior, string nome_da_parada, string horario_chegada, string horario_de_partida)
     {
-        // buscar parada anterior ao ponto em que a nova parada será adicionada
-        // criar e adicionar uma nova parada após a parada anteriro
         if (indice_anterior > this->tamanho_)
         {
             cout << "Índice de parada fora do intervalo de paradas." << endl;
             return;
         }
 
-        // dá para fazer mais rápido caso o usuário escolha inserir após última parada
-        // e se ele quiser inserir no início, como faz?
-
-        NoDuplamenteEncadeavel *no_iterador = nullptr;
-        if (indice_anterior == this->tamanho_)
+        NoDuplamenteEncadeavel *no_iterador = this->no_sentinela_;
+        unsigned counter = 1;
+        while (counter < indice_anterior)
         {
-            no_iterador = this->ultimo_no_;
-        }
-        else
-        {
-            no_iterador = this->primeiro_no_;
-            unsigned counter = 1;
-            while (counter < indice_anterior)
-            {
-                no_iterador = no_iterador->proximo_no;
-                counter++;
-            }
+            no_iterador = no_iterador->proximo_no;
+            counter++;
         }
 
         NoDuplamenteEncadeavel *nova_parada = new NoDuplamenteEncadeavel(nome_da_parada, horario_de_partida, horario_chegada);
@@ -65,9 +49,18 @@ ele quer inserir uma parada e inserirá a parada.
         no_iterador->proximo_no = nova_parada;
     }
 
+    void AdicionaPrimeiraParada(string nome, string chegada, string partida)
+    {
+        NoDuplamenteEncadeavel *primeira_parada = new NoDuplamenteEncadeavel(nome, partida, chegada);
+        primeira_parada->no_anterior = primeira_parada;
+        primeira_parada->proximo_no = primeira_parada;
+        this->no_sentinela_ = primeira_parada;
+        this->tamanho_++;
+    }
+
     void ImprimeParadasNumeradas() const
     {
-        NoDuplamenteEncadeavel *no_iterador = this->primeiro_no_;
+        NoDuplamenteEncadeavel *no_iterador = this->no_sentinela_;
         unsigned contador = 1;
         while (no_iterador != nullptr)
         {
@@ -90,8 +83,8 @@ ele quer inserir uma parada e inserirá a parada.
     {
         while (tamanho_ > 0)
         {
-            NoDuplamenteEncadeavel *auxiliar = primeiro_no_;
-            primeiro_no_ = primeiro_no_->proximo_no;
+            NoDuplamenteEncadeavel *auxiliar = no_sentinela_;
+            no_sentinela_ = no_sentinela_->proximo_no;
             delete auxiliar;
             tamanho_--;
         }
