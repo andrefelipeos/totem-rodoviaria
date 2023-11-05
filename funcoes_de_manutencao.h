c06ddecd09e5fcaf1625c1ed9563d968d38ded7e
André Sousa <andre.felipe.2048@gmail.com>
2023-11-05 11:23:07 -0300

    Cria header com funções de manutenção e ordena as funções

---
+++ funcoes_de_manutencao.h
@@ -0,0 +1,18 @@
+#ifndef FUNCOES_DE_MANUTENCAO_H
+#define FUNCOES_DE_MANUTENCAO_H
+
+#include "no_duplamente_encadeavel.h"
+
+void AdicionarLinha();
+void AdicionarParada();
+void AlterarParada();
+bool AutenticaUsuario();
+NoDuplamenteEncadeavel *CriarNovaParada();
+void MostrarTodasAsLinhas();
+void OpcoesDeManutencao();
+void ProcurarLinhaDeOnibus();
+void RealizarManutencao();
+void RemoverLinha();
+void RemoverParada();
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "funcoes_de_manutencao.h"
 #include "lista_encadeada.h"
 
 #include <iostream>
@@ -6,18 +7,6 @@
 using namespace std;
 
 const char *SENHA_PARA_MANUTENCAO = "senhafraca";
-
-void AdicionarLinha();
-void AdicionarParada();
-void AlterarParada();
-bool AutenticaUsuario();
-NoDuplamenteEncadeavel *CriarNovaParada();
-void MostrarTodasAsLinhas();
-void OpcoesDeManutencao();
-void RealizarManutencao();
-void ProcurarLinhaDeOnibus();
-void RemoverLinha();
-void RemoverParada();
 
 ListaEncadeada linhas_de_onibus;
 
@@ -48,120 +37,6 @@ int main()
     linhas_de_onibus.ImprimeDadosFormatados();
 
     return 0;
-}
-
-void AlterarParada()
-{
-    cout << "De qual linha você quer alterar uma parada?" << endl;
-    cout << "Número da linha: ";
-    unsigned numero_da_linha;
-    cin >> numero_da_linha;
-    NoEncadeavel *linha = linhas_de_onibus.BuscaLinhaPeloNumero(numero_da_linha);
-    if (linha == nullptr)
-    {
-        cout << "Indique uma linha cadastrada." << endl;
-        return;
-    }
-    if (linha->paradas->EstaVazia())
-    {
-        cout << "A lista de paradas da linha " << linha->numero_da_linha
-             << " - " << linha->nome_da_companhia
-             << " está vazia. Não há paradas para alterar." << endl;
-        return;
-    }
-    linha->paradas->ImprimeParadasNumeradas();
-    cout << "Qual parada você quer alterar?" << endl;
-    cout << "Índice da parada: ";
-    unsigned indice_da_parada;
-    cin >> indice_da_parada;
-    if (indice_da_parada > linha->paradas->RecuperaTamanho() || indice_da_parada < 1)
-    {
-        cout << "Índice inválido. Escolha uma parada entre " << linha->paradas->RecuperaTamanho() << " parada(s) existente(s)." << endl;
-        return;
-    }
-    cout << "Novo nome da parada: ";
-    string novo_nome;
-    getline(cin >> ws, novo_nome);
-    cout << "Novo horário de chegada: ";
-    string novo_horario_de_chegada;
-    getline(cin >> ws, novo_horario_de_chegada);
-    cout << "Novo horário de partida: ";
-    string novo_horario_de_partida;
-    getline(cin >> ws, novo_horario_de_partida);
-    linha->paradas->AlteraParadaPeloIndice(indice_da_parada, novo_nome, novo_horario_de_chegada, novo_horario_de_partida);
-}
-
-void MostrarTodasAsLinhas()
-{
-    linhas_de_onibus.ImprimeDadosFormatados();
-}
-
-void ProcurarLinhaDeOnibus()
-{
-    cout << "Essa função ainda não foi implementada..." << endl;
-}
-
-void OpcoesDeManutencao()
-{
-    cout << "O que você deseja fazer?" << endl;
-    cout << "  0. Encerrar manutenção do programa" << endl;
-    cout << "  1. Adicionar uma linha de ônibus" << endl;
-    cout << "  2. Mostrar todas as linhas de ônibus" << endl;
-    cout << "  3. Remover uma linha de ônibus" << endl;
-    cout << "  4. Adicionar uma parada em uma linha" << endl;
-    cout << "  5. Alterar uma parada em uma linha" << endl;
-    cout << "  6. Remover uma parada em uma linha" << endl;
-}
-
-bool AutenticaUsuario()
-{
-    string senha_do_usuario;
-    cout << "Senha para manutenção: ";
-    getline(cin >> ws, senha_do_usuario);
-    if (senha_do_usuario == SENHA_PARA_MANUTENCAO)
-    {
-        return true;
-    }
-    return false;
-}
-
-void RealizarManutencao()
-{
-    bool usuario_autenticado = AutenticaUsuario();
-    if (!usuario_autenticado)
-    {
-        return;
-    }
-
-    int escolha = -1;
-    do
-    {
-        OpcoesDeManutencao();
-        cin >> escolha;
-        switch (escolha)
-        {
-        case 1:
-            AdicionarLinha();
-            break;
-        case 2:
-            MostrarTodasAsLinhas();
-            break;
-        case 3:
-            RemoverLinha();
-            break;
-        case 4:
-            AdicionarParada();
-            break;
-        case 5:
-            AlterarParada();
-            break;
-        case 6:
-            RemoverParada();
-            break;
-        default:
-            break;
-        }
-    } while (escolha != 0);
 }
 
 void AdicionarLinha()
@@ -208,6 +83,59 @@ void AdicionarParada()
     }
 }
 
+void AlterarParada()
+{
+    cout << "De qual linha você quer alterar uma parada?" << endl;
+    cout << "Número da linha: ";
+    unsigned numero_da_linha;
+    cin >> numero_da_linha;
+    NoEncadeavel *linha = linhas_de_onibus.BuscaLinhaPeloNumero(numero_da_linha);
+    if (linha == nullptr)
+    {
+        cout << "Indique uma linha cadastrada." << endl;
+        return;
+    }
+    if (linha->paradas->EstaVazia())
+    {
+        cout << "A lista de paradas da linha " << linha->numero_da_linha
+             << " - " << linha->nome_da_companhia
+             << " está vazia. Não há paradas para alterar." << endl;
+        return;
+    }
+    linha->paradas->ImprimeParadasNumeradas();
+    cout << "Qual parada você quer alterar?" << endl;
+    cout << "Índice da parada: ";
+    unsigned indice_da_parada;
+    cin >> indice_da_parada;
+    if (indice_da_parada > linha->paradas->RecuperaTamanho() || indice_da_parada < 1)
+    {
+        cout << "Índice inválido. Escolha uma parada entre " << linha->paradas->RecuperaTamanho() << " parada(s) existente(s)." << endl;
+        return;
+    }
+    cout << "Novo nome da parada: ";
+    string novo_nome;
+    getline(cin >> ws, novo_nome);
+    cout << "Novo horário de chegada: ";
+    string novo_horario_de_chegada;
+    getline(cin >> ws, novo_horario_de_chegada);
+    cout << "Novo horário de partida: ";
+    string novo_horario_de_partida;
+    getline(cin >> ws, novo_horario_de_partida);
+    linha->paradas->AlteraParadaPeloIndice(indice_da_parada, novo_nome, novo_horario_de_chegada, novo_horario_de_partida);
+}
+
+bool AutenticaUsuario()
+{
+    string senha_do_usuario;
+    cout << "Senha para manutenção: ";
+    getline(cin >> ws, senha_do_usuario);
+    if (senha_do_usuario == SENHA_PARA_MANUTENCAO)
+    {
+        return true;
+    }
+    return false;
+}
+
 NoDuplamenteEncadeavel *CriarNovaParada()
 {
     string nome_da_parada;
@@ -220,6 +148,67 @@ NoDuplamenteEncadeavel *CriarNovaParada()
     cout << "Horário de partida: ";
     getline(cin >> ws, horario_de_partida);
     return new NoDuplamenteEncadeavel(nome_da_parada, horario_de_chegada, horario_de_partida);
+}
+
+void MostrarTodasAsLinhas()
+{
+    linhas_de_onibus.ImprimeDadosFormatados();
+}
+
+void OpcoesDeManutencao()
+{
+    cout << "O que você deseja fazer?" << endl;
+    cout << "  0. Encerrar manutenção do programa" << endl;
+    cout << "  1. Adicionar uma linha de ônibus" << endl;
+    cout << "  2. Mostrar todas as linhas de ônibus" << endl;
+    cout << "  3. Remover uma linha de ônibus" << endl;
+    cout << "  4. Adicionar uma parada em uma linha" << endl;
+    cout << "  5. Alterar uma parada em uma linha" << endl;
+    cout << "  6. Remover uma parada em uma linha" << endl;
+}
+
+void ProcurarLinhaDeOnibus()
+{
+    cout << "Essa função ainda não foi implementada..." << endl;
+}
+
+void RealizarManutencao()
+{
+    bool usuario_autenticado = AutenticaUsuario();
+    if (!usuario_autenticado)
+    {
+        return;
+    }
+
+    int escolha = -1;
+    do
+    {
+        OpcoesDeManutencao();
+        cin >> escolha;
+        switch (escolha)
+        {
+        case 1:
+            AdicionarLinha();
+            break;
+        case 2:
+            MostrarTodasAsLinhas();
+            break;
+        case 3:
+            RemoverLinha();
+            break;
+        case 4:
+            AdicionarParada();
+            break;
+        case 5:
+            AlterarParada();
+            break;
+        case 6:
+            RemoverParada();
+            break;
+        default:
+            break;
+        }
+    } while (escolha != 0);
 }
 
 void RemoverLinha()
